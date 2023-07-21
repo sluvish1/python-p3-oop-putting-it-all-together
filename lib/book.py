bb45794710887ef87f761d226d696157b0e3ee94
Vaschell Mkenzie <mvaschell@gmail.com>
2023-07-21 14:38:32 -0400

    finished

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self._page_count = page_count
+        self.title = title
+        # if page_count != int: print("page_count must be an integer")
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self,page_count):
+        if page_count != int:
+            print("page_count must be an integer")
+        else: 
+            self._page_count == int    
     
-        
+    page_count = property(get_page_count,set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+ # age = property(get_age, set_age,)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self._size = size
+    
+    def get_shoe_size(self):
+        return self._size
+
+    def set_shoe_size(self,size):
+        if size != int:
+            print("size must be an integer")
+        else:
+            self._size == int
+    size = property(get_shoe_size,set_shoe_size)
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
